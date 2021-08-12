bfd386f39ed526c5c826110beb5cb44f3faaf080
Gambra <droneitaly.ga@gmail.com>
2021-08-12 11:18:52 +0200

    creation of Backend

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+require('dotenv').config();
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: process.env.DATABASE_HOST,
+		user: process.env.DATABASE_USERNAME,
+		password: process.env.DATABASE_PASSWORD,
+		database: process.env.DATABASE,
+	},
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// CORS implemented so that we don't get errors when trying to access the server from a different server location
+app.use(cors());
+
+// TODO create CRUD operations
+// example of crud operations:
+
+// GET: Fetch all users from the database
+app.get('/', (req, res) => {
+	db.select('*')
+		.from('utenti')
+		.then((data) => {
+			console.log(data);
+			res.json(data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}, http://localhost:${port}`));
