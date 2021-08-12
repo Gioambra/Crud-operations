1c499d3187b5bbfb73ca89f1aa8782a3b5952e76
Gambra <droneitaly.ga@gmail.com>
2021-08-12 16:49:58 +0200

    Index page show credential only for now(test)

--- mycrud/src/ShowAllUser.js
+++ mycrud/src/ShowAllUser.js
@@ -1,9 +1,28 @@
-
+import React, { useState, useEffect } from 'react';
 export default function ShowAllUSer() {
 	// Here we call API
-	fetch('http://localhost:5000')
-		.then(response => response.json())
-		.then(data => console.log(data))
+	useEffect(() => {
 
-	return ("fetch data")
+		const getApi = () => {
+			fetch('http://localhost:5000')
+				.then(response => response.json())
+				.then((data) => {
+					console.log(data)
+					setApiData(data);
+				});
+		}
+
+		getApi()
+	}, []);
+
+	const [apiData, setApiData] = useState([]);
+	console.log("APIDATA->", apiData)
+	return (
+		<div id="parent">
+			{apiData.map((utente, index) => {
+				return (<div key={index} ><h1>{utente.username}</h1> <h1>{utente.password}</h1> </div>)
+			})
+			}
+		</div>
+	)
 }
