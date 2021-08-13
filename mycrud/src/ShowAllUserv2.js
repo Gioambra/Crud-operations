28910c7f2e432d9d976dbd00c6f093ff21ddd671
Gambra <droneitaly.ga@gmail.com>
2021-08-13 10:40:38 +0200

    Handling username and password

--- mycrud/src/ShowAllUserv2.js
+++ mycrud/src/ShowAllUserv2.js
@@ -1,10 +1,22 @@
 
-
 function ShowAllUserv2(userName, password, apiData) {
 	console.log("Check->", userName, password)
 	console.log("ApiData->", apiData)
 
-}
+	// Controls on inputs credentials
+	apiData.forEach(user => {
+		if (userName === user.username) {
+			// username exist in the database
 
+			// so now we have to control if the password match
+			if (password === user.password) {
+				console.log("you are now logged")
+				// TODO create another page
+			}
+
+		}
+	});
+
+}
 
 export default ShowAllUserv2
