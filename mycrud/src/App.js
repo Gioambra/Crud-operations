2320449712e56d26ae4d6f59225f3eb856194faa
Gambra <droneitaly.ga@gmail.com>
2021-08-13 10:32:24 +0200

    Add ShowAllUserv2(receive credentials APIDATA)

--- mycrud/src/App.js
+++ mycrud/src/App.js
@@ -1,18 +1,41 @@
 import './App.css';
 
 import { Flex, FormLabel, FormControl, Input, Button } from "@chakra-ui/react"
-import Check from './check';
-import ShowAllUser from "./ShowAllUser"
-
+//import Check from './check';
+//import ShowAllUser from "./ShowAllUser"
+import ShowAllUserv2 from "./ShowAllUserv2"
+import React, { useEffect, useState } from "react"
 /*
   Create 2 varibales passowrd and username, on click on button Submit these variables will be send
   to Check component
 */
 function App() {
+
+  // Here we call API
+  useEffect(() => {
+
+    const getApi = () => {
+      fetch('http://localhost:5000')
+        .then(response => response.json())
+        .then((data) => {
+          console.log(data)
+          setApiData(data);
+        });
+    }
+
+    getApi()
+  }, []);
+
+  const [apiData, setApiData] = useState([]);
+  console.log("APIDATA->", apiData)
+
+
+
   var password
   var userName
   return (
-    <Flex direction="column">
+
+    <Flex direction="column" >
       <FormControl id="userName">
         <FormLabel>Email address</FormLabel>
         <Input type="username" id="user" onChange={(event) => userName = event.target.value} />
@@ -21,9 +44,9 @@ function App() {
         <FormLabel>password</FormLabel>
         <Input type="password" id="psw" onChange={(event) => password = event.target.value} />
       </FormControl>
-      <Button onClick={() => Check(userName, password)}>Submit</Button>
+      <Button onClick={() => ShowAllUserv2(userName, password, apiData)}>Submit</Button>
 
-      <ShowAllUser></ShowAllUser>
+
     </Flex>
   );
 }
---
+++ mycrud/src/ShowAllUserv2.js
@@ -0,0 +1,10 @@
+
+
+function ShowAllUserv2(userName, password, apiData) {
+	console.log("Check->", userName, password)
+	console.log("ApiData->", apiData)
+
+}
+
+
+export default ShowAllUserv2
