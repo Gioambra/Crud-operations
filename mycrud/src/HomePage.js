7817db0037c2e75b4fe5e4129592156a87f9fb67
Gambra <droneitaly.ga@gmail.com>
2021-08-16 17:37:19 +0200

    title in the hompage

--- mycrud/src/HomePage.js
+++ mycrud/src/HomePage.js
@@ -1,6 +1,7 @@
 import {
 	Button,
-	Text
+	Text,
+	Box
 } from "@chakra-ui/react"
 
 import { useHistory } from 'react-router-dom'
@@ -9,9 +10,26 @@ export default function HomePage() {
 	console.log("username from homepage->>")
 	let history = useHistory();
 	return (
-		<div>HomePage
+
+		<Box w="100%"
+			h="1000px"
+			bgGradient={[
+				"linear(to-tr, yellow.300,blue.400)",
+				"linear(to-t, blue.200, teal.500)",
+				"linear(to-b, green.100, blue.300)",
+			]}>
+			<Text
+				position="center"
+				bgGradient="linear(to-l, #7928CA,#FF0080)"
+				bgClip="text"
+				fontSize="6xl"
+				fontWeight="extrabold"
+			>
+				Welcome to the HomePage!
+			</Text>
+
 			<Button onClick={() => history.push("/Update")} >Update your account</Button>
 			<Text> you are now logged with</Text>
-		</div>
+		</Box>
 	)
 }
