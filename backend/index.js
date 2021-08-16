3f79833c9fd66b244ad71277b59bf9b33d34d9dc
Gambra <droneitaly.ga@gmail.com>
2021-08-16 09:49:39 +0200

    Handling creation of new user

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,24 @@ app.get('/', (req, res) => {
 		});
 });
 
+// POST: Create movies and add them to the database
+app.post('/newAccount', (req, res) => {
+	const { id, username, password } = req.body;
+	db.insert({
+		id: id,
+		username: username,
+		password: password,
+	})
+		.from('public.utenti')
+		.then(() => {
+			console.log('User Added');
+			return res.json({ msg: 'User Created' });
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}, http://localhost:${port}`));
--- mycrud/src/CreateNewAccount.js
+++ mycrud/src/CreateNewAccount.js
@@ -1,22 +1,48 @@
 import * as React from "react"
 import {
+	Button,
 	Flex,
 	FormControl,
 	FormLabel,
 	Input,
 	Text
 } from "@chakra-ui/react"
+
+
 export default function CreateNewAccount() {
-	console.log("hero we go")
+	var password
+	var userName
+
 	return (
-		<Flex>
+		<Flex direction="column">
 			<Text fontSize="4xl">Create New Account</Text>
+			<Flex my="2rem">
 			<FormControl id="username">
 				<FormLabel>Username</FormLabel>
-				<Input type="username" />
+					<Input type="username" onChange={(event) => userName = event.target.value} />
 				<FormLabel>password</FormLabel>
-				<Input type="password" />
-			</FormControl>
+					<Input type="password" onChange={(event) => password = event.target.value} />
+				</FormControl>
+			</Flex>
+			<Button onClick={() => Create(userName, password)}>Create</Button>
 		</Flex>
 	)
+}
+
+function Create(username, password) {
+	console.log("Creazione account->>", username, password)
+
+	var randomId = Math.round(Math.random() * 800);
+	// Simple POST request with a JSON body using fetch
+	const requestOptions = {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			id: randomId,
+			username: username,
+			password: password
+		})
+	};
+	fetch('http://localhost:5000/newAccount', requestOptions)
+		.then(response => response.json())
 }
